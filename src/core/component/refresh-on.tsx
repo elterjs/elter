0d93da1d2e27e95d0c1b5b618c77346df11dc4d4
S.Watanabe <ocelot2828@gmail.com>
2024-10-31 22:57:46 +0900

    fix(refresh-on.tsx): remove freaky loop code

--- src/core/component/refresh-on.tsx
+++ src/core/component/refresh-on.tsx
@@ -1,60 +1,26 @@
 'use client';
 
-import { useEffect, useTransition } from 'react';
+import { useEffect } from 'react';
 import { useRouter, usePathname } from 'next/navigation';
 
 export const RefreshOn = () => {
   const router = useRouter();
   const pathname = usePathname();
-  const [, startTransition] = useTransition();
 
   useEffect(() => {
     router.refresh();
     const handleClick = (e: MouseEvent) => {
       const target = e.target as HTMLElement;
       const targetAnchor = target.closest('a');
-      if (pathname && targetAnchor instanceof HTMLAnchorElement && targetAnchor.origin === window.location.origin) router.refresh();
-    };
-    let isRefreshing = false; // Debounce processing
-    const observeStyleSheets = () => {
-      const styleObserver = new MutationObserver(mutations => {
-        if (isRefreshing) return;
-        for (const mutation of mutations) {
-          if (mutation.type === 'childList') {
-            const addedNodes = Array.from(mutation.addedNodes);
-            if (
-              addedNodes.some(
-                node =>
-                  node instanceof HTMLStyleElement || (node instanceof HTMLLinkElement && !(node.rel === 'preload' && node.getAttribute('as') === 'style'))
-              )
-            ) {
-              startTransition(() => {
-                isRefreshing = true;
-                router.refresh();
-                setTimeout(() => {
-                  isRefreshing = false;
-                }, 350);
-              });
-              break;
-            }
-          }
-        }
-      });
-
-      styleObserver.observe(document.head, {
-        childList: true,
-        subtree: false,
-      });
-
-      return styleObserver;
+      if (pathname && targetAnchor instanceof HTMLAnchorElement && targetAnchor.origin === window.location.origin) {
+        router.refresh();
+      }
     };
 
     document.addEventListener('click', handleClick);
-    const cssObserver = observeStyleSheets();
 
     return () => {
       document.removeEventListener('click', handleClick);
-      cssObserver.disconnect();
     };
   }, [router, pathname]);
 
